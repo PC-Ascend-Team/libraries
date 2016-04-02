a45f7501092ed033232c6eb8a94ec609721e7770
jcahal <jacahal@gmail.com>
2016-04-01 23:01:36 -0700

    remove BMP and GYRO from IMU

--- Adafruit_10DOF/Adafruit_10DOF.h
+++ Adafruit_10DOF/Adafruit_10DOF.h
@@ -23,8 +23,8 @@
 
 #include <Adafruit_Sensor.h>
 #include <Adafruit_LSM303_U.h>
-#include <Adafruit_BMP085_U.h>
-#include <Adafruit_L3GD20_U.h>
+//#include <Adafruit_BMP085_U.h>
+//#include <Adafruit_L3GD20_U.h>
 #include <Wire.h>
 
 /** Sensor axis */
